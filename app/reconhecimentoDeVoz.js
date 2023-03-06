fa7d7c01d00fc2e36fa23858226a0067eb5d99b8
Pedro Teixeira <pedro.teixeira@gruponsc.com.br>
2023-03-06 15:00:15 -0300

    Update 1.0

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,24 @@
+const elementoChute = document.querySelectorById
+
+window.SpeechRecognition = window.SpeechRecognition || 
+webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start();
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e){
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    VerificaChuteValido(chute)
+}
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+        <div>Voce disse:</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start())
---
+++ app/validacao.js
@@ -0,0 +1,45 @@
+function VerificaChuteValido(chute){
+    const numero = +chute
+
+    if(NumeroForReal()(numero)){
+        elementoChute.innerHTML += '<div>"Valor Invalido"</div>'
+    }
+
+    if(VerificaMaiorMenor(numero)){
+        elementoChute.innerHTML += `
+        <div>Fale um numero deve estar entre ${menorValor}
+         e ${maiorValor} </div>
+        `
+        return
+    }
+
+    if (numero === numero){
+        document.body.innerHTML = `
+            <h2>Voce acertou!</h2>
+            <h3>O numero secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    }else if(numero > numeroSecreto){
+        elementoChute.innerHTML +=`
+        <div>O numero secreto é menor<i class="fa-solid fa-angle-down"></i></div>
+        `
+    }else{
+        elementoChute.innerHTML +=`
+        <div>O numero secreto é maior<i class="fa-solid fa-angle-up"></i></div>
+        `
+    }
+}
+
+function NumeroForReal(numero) {
+    return Number.isNaN(numero);
+}
+function VerificaMaiorMenor(numero){
+    return numero > maiorValor || numero < menorValor
+
+}
+documento.body.addEventListener('click', e =>{
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload();
+    }
+})
